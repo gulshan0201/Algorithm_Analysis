2f1705d912206d39012e98c4abb57c41ed8422d9
N Gulshan Kumar <84889171+gulshan0201@users.noreply.github.com>
2025-09-19 17:27:41 +0000

    topo

---
+++ topological_sort.py
@@ -0,0 +1,33 @@
+def topologicalSortUtil(v, adj, visited, stack):
+# Mark the current node as visited
+    visited[v] = True
+# Recur for all adjacent vertices
+    for i in adj[v]:
+        if not visited[i]:
+            topologicalSortUtil(i, adj, visited, stack)
+# Push current vertex to stack which stores the result
+    stack.append(v)
+# Function to perform Topological Sort
+def topologicalSort(adj, V):
+# Stack to store the result
+    stack = []
+    visited = [False] * V
+# Call the recursive helper function to store
+# Topological Sort starting from all vertices one by
+# one
+    for i in range(V):
+        if not visited[i]:
+            topologicalSortUtil(i, adj, visited, stack)
+# Print contents of stack
+    print("Topological sorting of the graph:", end=" ")
+    while stack:
+        print(stack.pop(), end=" ")
+# Number of nodes
+V = 6
+# Edges
+edges = [[5, 0], [4, 0], [4, 1], [3, 1], [2, 3], [5, 2]]
+# Graph represented as an adjacency list
+adj = [[] for _ in range(V)]
+for i in edges:
+    adj[i[0]].append(i[1])
+topologicalSort(adj, V)
